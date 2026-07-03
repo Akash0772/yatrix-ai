2f901980faf25efd28b849fc47a8ce9b9fd71bf0
Akash Chaurasiya <akashchaurasia617@gmail.com>
2026-07-03 13:24:15 +0530

    update1 file chat.js

--- api/chat.js
+++ api/chat.js
@@ -26,7 +26,8 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent`,
+      
       {
         method: 'POST',
         headers: {
